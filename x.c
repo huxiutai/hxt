0b36bed3a4785155949daabef80c144dc11fdf40
huxiutai <huxiutai@163.com>
2019-05-11 18:00:22 +0800

    q4

--- x.c
+++ x.c
@@ -1,5 +1,6 @@
 Git is a distributed version control system.
-2.Git is free software distributed under the GPL. 
+2.Git is free software distributed under the GPL.
+3.Git has a mutable index called stage. 
 
 
 
